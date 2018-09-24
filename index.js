bde17102150a54885488d8f4e46be97f5564705d
Nir Adler <niradler@users.noreply.github.com>
2018-09-24 20:19:52 +0300

    microservice access with express (#1)

--- index.js
+++ index.js
@@ -1,76 +1,30 @@
 require('dotenv').config()
-const helpers = require('./helpers')
 const moment = require('moment');
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const getResources = require('./getResources');
+const app = express();
+const {
+    SOURCE,
+    FROM = moment().format('MM-YYYY'),
+    DELAY = 100
+} = process.env;
 
-let {SOURCE, FROM=moment().format('MM-YYYY'), DELAY=50} = process.env;
-const months = helpers.initMonths(FROM);
 
-if (months.length === 0) {
-    throw new Error('Date is After!')
-}
+app.use(cors());
+app.use(bodyParser.json());
 
-const delay = (t) => new Promise((resolve) => setTimeout(() => resolve(''), t));
+app.get('/resources', async (req, res) => {
+    const {
+        from = FROM, source = SOURCE
+    } = req.query;
+    const response = await getResources(from, source, DELAY).catch(e => res.json({
+        err: e.message
+    }));
 
-const cacheImages = async(images) => {
-    const imagesPromises = images.map((uri) => helpers.downloadImage(uri, uri.slice(uri.lastIndexOf('/') + 1)))
-    return Promise
-        .all(imagesPromises)
-        .then(d => console.log('images saved'))
-        .catch(e => console.log('images failed', e))
-}
+    return res.json(response);
+})
 
-const init = async() => {
-    let data = [];
-
-    try {
-        for (let i in months) {
-            const m = months[i];
-            const titles = [];
-            const images = [];
-            
-            try {
-                console.log('fetching... ', SOURCE + m);
-                const $ = await helpers.getHtml(SOURCE + m, m);
-
-                $('figure > span > img').each((i, elem) => {
-                    images[i] = elem.attribs['data-lazy-src'];
-                });
-    
-                await cacheImages(images);
-    
-                $('h4 > a').each((i, elem) => {
-                    titles[i] = {
-                        text: elem.children[0].data,
-                        link: elem.attribs['href'],
-                        image: images[i].slice(images[i].lastIndexOf('/') + 1),
-                        description: ''
-                    }
-                });
-    
-                $('h4 + p').each((i, elem) => {
-                    titles[i].description = elem
-                        .children
-                        .map(c => c.data)
-                        .join();
-    
-                });
-    
-                data = [
-                    ...data,
-                    ...titles
-                ];
-            } catch (e) {
-                console.log('fetching failed', e);
-            }
-           
-            await delay(DELAY);
-        };
-
-        return data;
-    } catch (e) {
-        console.log('process failed', e);
-    }
-}
-
-console.time('start')
-init().then(d => console.timeEnd('start'));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log('App listening on port ' + PORT))
